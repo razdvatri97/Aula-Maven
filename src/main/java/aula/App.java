bbeb4d96493ea2b4d9a708b4e85970845671a55a
Jonathan <jonas.ca2@gmail.com>
2020-03-29 20:25:27 -0300

    Primeiro Commit de Jonathan

--- src/main/java/aula/App.java
+++ src/main/java/aula/App.java
@@ -36,6 +36,8 @@ public class App {
                 System.out.println("Cor : " + cor);
                 System.out.println("Categoria : " + categoria);
                 System.out.println("---------------\n\n");
+
+                System.out.println("Alteração de Jonathan");
             }
         }
     }
